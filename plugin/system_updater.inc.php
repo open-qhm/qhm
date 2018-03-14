345a698f6bc49b92c8d2bebae7bedb6837083111
Shun Hotta <stars.big2men@gmail.com>
2018-03-15 00:02:31 +0900

    Exclude circle ci config dir on update

--- plugin/system_updater.inc.php
+++ plugin/system_updater.inc.php
@@ -252,7 +252,8 @@ function plugin_system_updater_move($source, $dist)
 					)|(?: # 前方一致
 						(?:
 							attach|diff|backup|counter|cache|cacheqhm|cacheqblog|
-							skin\/hokukenstyle\/(.+)|swfu\/d|swfu\/data|trackback|wiki
+							skin\/hokukenstyle\/(.+)|swfu\/d|swfu\/data|trackback|wiki|
+							.circleci
 						)
 						\/
 					)
