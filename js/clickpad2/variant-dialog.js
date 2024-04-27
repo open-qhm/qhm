080da67e6c75104e61174cb11cd39c86e6dc62e9
Shun Hotta <stars.big2men@gmail.com>
2024-04-27 23:11:50 +0900

    装飾モーダル

--- js/clickpad2/variant-dialog.js
+++ js/clickpad2/variant-dialog.js
@@ -25,6 +25,7 @@ export const makeButtonVariantDialog = (buttonId, buttonDefinition) => {
     content.classList.add('clickpad2__dialog-content')
     buttonDefinition.dialog.forEach(({ message, option }, index) => {
       const wrapper = document.createElement('div')
+      wrapper.classList.add('clickpad2__dialog-item')
 
       const id = `dialog-control-${index + 1}`
 
@@ -69,9 +70,10 @@ export const makeButtonVariantDialog = (buttonId, buttonDefinition) => {
           content.appendChild(label)
 
           // option.values ごとに label>input を生成する
-          option.values.forEach(({ label, color, value, checked }, index2) => {
+          option.values.forEach(({ label, color, icon, value, checked }, index2) => {
             const _id = `${id}-${index2 + 1}`
             const labelElement = document.createElement('label')
+            labelElement.classList.add('clickpad2__dialog-radio-item-label')
             const input = document.createElement('input')
             input.id = _id
             input.type = 'radio'
@@ -80,12 +82,24 @@ export const makeButtonVariantDialog = (buttonId, buttonDefinition) => {
             input.checked = checked
             labelElement.appendChild(input)
             if (color !== undefined) {
+              labelElement.classList.add('clickpad2__dialog-radio-item-label--color')
               // 色名をlabel, color を背景色にする
               const colorBox = document.createElement('span')
               colorBox.classList.add('clickpad2__dialog-radio-color-box')
               colorBox.title = label
               colorBox.style.backgroundColor = color
               labelElement.appendChild(colorBox)
+            } else if (icon !== undefined) {
+              labelElement.classList.add('clickpad2__dialog-radio-item-label--icon')
+              // icon をラベルの前に配置する
+              const iconElement = document.createElement('span')
+              iconElement.classList.add('material-icons-outlined')
+              iconElement.textContent = icon
+              labelElement.appendChild(iconElement)
+              const text = document.createElement('span')
+              text.textContent = label
+              text.classList.add('clickpad2__dialog-radio-item-label-text')
+              labelElement.appendChild(text)
             } else {
               labelElement.appendChild(document.createTextNode(label))
             }
@@ -114,6 +128,125 @@ export const makeButtonVariantDialog = (buttonId, buttonDefinition) => {
             wrapper.appendChild(labelElement)
             content.appendChild(wrapper)
           })
+          break
+        }
+        case 'deco-font': {
+          const item = document.createElement('div')
+          item.classList.add('clickpad2__dialog-deco-font-item')
+
+          const fontStyles = [
+            { name: 'bold', label: '太字', value: 'b' },
+            { name: 'underline', label: '下線', value: 'u' },
+            { name: 'italic', label: '斜体', value: 'i' }
+          ]
+          fontStyles.forEach((fontStyle, index2) => {
+            const label = document.createElement('label')
+            label.classList.add('clickpad2__dialog-deco-font-item-label', `clickpad2__dialog-deco-font-item-label-${fontStyle.name}`)
+            const labelTitle = document.createElement('span')
+            labelTitle.textContent = fontStyle.label
+
+            const _id = `${id}-${index + 1}`
+            const input = document.createElement('input')
+            input.type = 'checkbox'
+            input.id = _id
+            input.name = id
+            input.value = fontStyle.value
+
+            label.appendChild(input)
+            label.appendChild(labelTitle)
+            item.appendChild(label)
+          })
+
+          wrapper.appendChild(item)
+          content.appendChild(wrapper)
+          break
+        }
+        case 'deco-color': {
+          const item = document.createElement('div')
+          item.classList.add('clickpad2__dialog-deco-color-item')
+          const label = document.createElement('label')
+          const title = document.createElement('span')
+          title.classList.add('clickpad2__dialog-deco-color-item-title')
+          title.textContent = '文字色'
+          const tip = document.createElement('span')
+          tip.classList.add('clickpad2__dialog-deco-color-item-tip')
+          tip.textContent = '（カラーコード/カラーネーム）'
+          label.appendChild(title)
+          label.appendChild(tip)
+          label.htmlFor = id
+          item.appendChild(label)
+
+          const input = document.createElement('input')
+          input.id = id
+          input.name = id
+          input.type = 'text'
+          item.appendChild(input)
+
+          wrapper.appendChild(item)
+          content.appendChild(wrapper)
+          break
+        }
+        case 'deco-bg-color': {
+          const item = document.createElement('div')
+          item.classList.add('clickpad2__dialog-deco-bg-color-item')
+          const label = document.createElement('label')
+          const title = document.createElement('span')
+          title.classList.add('clickpad2__dialog-deco-bg-color-item-title')
+          title.textContent = '背景色'
+          const tip = document.createElement('span')
+          tip.classList.add('clickpad2__dialog-deco-bg-color-item-tip')
+          tip.textContent = '（カラーコード/カラーネーム）'
+          label.appendChild(title)
+          label.appendChild(tip)
+          label.htmlFor = id
+          content.appendChild(label)
+
+          const input = document.createElement('input')
+          input.id = id
+          input.name = id
+          input.type = 'text'
+          item.appendChild(input)
+
+          wrapper.appendChild(item)
+          content.appendChild(wrapper)
+          break
+        }
+        case 'deco-font-size': {
+          const item = document.createElement('div')
+          item.classList.add('clickpad2__dialog-deco-font-size-item')
+          const label = document.createElement('label')
+          const title = document.createElement('span')
+          title.classList.add('clickpad2__dialog-deco-font-size-item-title')
+          title.textContent = '文字サイズ'
+          const tip = document.createElement('span')
+          tip.classList.add('clickpad2__dialog-deco-font-size-item-tip')
+          tip.textContent = '（数値/em/キーワード）'
+          label.appendChild(title)
+          label.appendChild(tip)
+          label.htmlFor = id
+          item.appendChild(label)
+
+          const input = document.createElement('input')
+          input.id = id
+          input.name = id
+          input.type = 'text'
+          item.appendChild(input)
+
+          // 凡例
+          const legend = document.createElement('div')
+          legend.classList.add('clickpad2__dialog-deco-font-size-item-legend')
+          const legendTitle = document.createElement('span')
+          legendTitle.textContent = '[ 文字サイズ指定キーワード ]'
+          legend.appendChild(legendTitle)
+          const br = document.createElement('br')
+          legend.appendChild(br)
+          const legendTip = document.createElement('span')
+          legendTip.textContent = 'xx-small / x-small / small / medium（初期値）/ large / x-large / xx-large'
+          legend.appendChild(legendTip)
+          item.appendChild(legend)
+
+          wrapper.appendChild(item)
+          content.appendChild(wrapper)
           break
         }
       }
