fca6c57242ea2def158b8ab44d330ea50e1a6b77
Shun Hotta <stars.big2men@gmail.com>
2024-05-29 16:01:48 +0900

    Google Chart API 経由でQRコードを取得できなくなったため代替手段としてJSで生成する

--- plugin/qr.inc.php
+++ plugin/qr.inc.php
@@ -12,7 +12,7 @@
  *
  *   USAGE:
  *     inline plugin:
- *       &qr(string[, width[, height[,encoding]]);
+ *       &qr(string[, width[, height]);
  *     action plugin:
  *       display QR code
  *     display display QR code URL
@@ -26,21 +26,13 @@ define('PLUGIN_QR_DEFAULT_SIZE', 150);
 
 function plugin_qr_inline() {
 
-	global $script, $vars;
+	global $script, $vars, $qt;
 	$page = $vars['page'];
 	$args = func_get_args();
 	//first param is string
 	$str = array_shift($args);
 
 	$w = $h = PLUGIN_QR_DEFAULT_SIZE;
-	$enc = '';
-	$encs = plugin_qr_get_encs();
-
-	if ($str == 'url') {
-		$url = $script. '?page='. rawurlencode($page). '&plugin=qr';
-
-		return str_replace('%URL%', $url, PLUGIN_QR_DESP_URL_FORMAT);
-	}
 
 	$sizecnt = 0;
 	foreach ($args as $arg) {
@@ -51,59 +43,20 @@ function plugin_qr_inline() {
 		else if ($sizecnt > 0 && preg_match('/^(\d+)$/', trim($arg))) {
 			$h = trim($arg);
 		}
-		else if (in_array($arg, $encs)) {
-			$enc = '&choe='. $arg;
-		}
 	}
 
-	$churl = plugin_qr_gen_qrcode($str, $w, $h, $enc);
+	$qt->appendv_once('plugin_qr_js', 'beforescript', '<script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js" integrity="sha512-CNgIRecGo7nphbeZ04Sc13ka07paqdeTu0WR1IM4kNcpmBAUSHSQX0FslNhTDadL4O5SAGapGt4FodqL8My0mA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript">
+$(function() {
+	$(".haik-plugin-qr-placeholder").each(function() {
+		var qrcode = new QRCode(this, {
+			text: $(this).data("url"),
+			width: Number($(this).data("width")),
+			height: Number($(this).data("height"))
+		});
+	});
+});
+</script>');
 
-	$chimg = str_replace('%CHURL%', $churl, PLUGIN_QR_DISP_FORMAT);
-
-	return $chimg;
-
-}
-
-function plugin_qr_action() {
-
-	global $script, $vars;
-	$page = $vars['page'];
-	$str = $script. '?'. rawurlencode($page);
-	$w = isset($vars['w'])? $vars['w']: PLUGIN_QR_DEFAULT_SIZE;
-	$h = isset($vars['h'])? $vars['h']: PLUGIN_QR_DEFAULT_SIZE;
-	$enc = isset($vars['enc']) && in_array($vars['enc'], plugin_qr_get_encs())? $vars['enc']: '';
-
-	$churl = plugin_qr_gen_qrcode($str, $w, $h, $enc);
-
-	if ($fp = fopen($churl, 'rb')) {
-		$qr = '';
-		while (!feof($fp)) {
-			$qr .= fread($fp, 8192);
-		}
-		fclose($fp);
-	}
-
-	$imgsize = strlen($qr);
-
-	//画像を出力
-	header('Content-Length: ' . $imgsize);
-	header('Content-Type: image/png');
-	echo $qr;
-	exit;
-}
-
-function plugin_qr_gen_qrcode($str, $w=PLUGIN_QR_DEFAULT_SIZE, $h=PLUGIN_QR_DEFAULT_SIZE, $enc='') {
-
-	$str = rawurlencode($str);
-	$ptns = array('%STRING%', '%WIDTH%', '%HEIGHT%', '%ENCODING%');
-	$rpls = array($str, $w, $h, $enc);
-	$churl = str_replace($ptns, $rpls, PLUGIN_QR_URL_FORMAT);
-
-	return $churl;
-
-
-}
-
-function plugin_qr_get_encs() {
-	return array('Shift_JIS', 'ISO-8859-1');
+	return '<div data-url="'. h($str) .'" data-width="'. h($w) .'" data-height="'. h($h) .'" class="haik-plugin-qr-placeholder"></div>';
 }
