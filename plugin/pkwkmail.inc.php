4a1773b6f9f7209dfd18226f38d8c3b00f28ba9d
Shun Hotta <stars.big2men@gmail.com>
2024-09-14 10:54:31 +0900

    配列追加と同時に文字列連結していたので修正

--- plugin/pkwkmail.inc.php
+++ plugin/pkwkmail.inc.php
@@ -309,7 +309,7 @@ function PKWKMAIL_formmaker($attr,$cnfm, $a_page)
 
 		//title - 各質問項目 - th
 		$k[0] = htmlspecialchars($k[0], ENT_QUOTES);
-		$render_title[] .= "\t".'<th class="style_th"><label for="PKWKMAIL_'.$attr_name[$n].'">'.str_replace('&amp;br;','<br />',$k[0]).$render_must.'</label></th>'."\n";
+		$render_title[] = "\t".'<th class="style_th"><label for="PKWKMAIL_'.$attr_name[$n].'">'.str_replace('&amp;br;','<br />',$k[0]).$render_must.'</label></th>'."\n";
 
 		//parts
 		$render_element_arr = $value_arr = array() ;
@@ -329,47 +329,47 @@ function PKWKMAIL_formmaker($attr,$cnfm, $a_page)
 		case 'text':
 		case 'email':
 		case 'password': //text,email,password
-			$render_element[] .= "\t".'<td class="style_td"><input id="PKWKMAIL_'.$attr_name[$n].'" name="PKWKMAIL_'.$attr_name[$n].'" type="text" value="'.$render_value[$n].'" size="'.$k[5][0].'" /></td>'."\n";
+			$render_element[] = "\t".'<td class="style_td"><input id="PKWKMAIL_'.$attr_name[$n].'" name="PKWKMAIL_'.$attr_name[$n].'" type="text" value="'.$render_value[$n].'" size="'.$k[5][0].'" /></td>'."\n";
 			break;
 		case 'textarea': //textarea
-			$render_element[] .= "\t".'<td class="style_td"><textarea id="PKWKMAIL_'.$attr_name[$n].'" name="PKWKMAIL_'.$attr_name[$n].'" cols="'.$k[5][0].'" rows="'.$k[5][1].'">'.$render_value[$n].'</textarea></td>'."\n";
+			$render_element[] = "\t".'<td class="style_td"><textarea id="PKWKMAIL_'.$attr_name[$n].'" name="PKWKMAIL_'.$attr_name[$n].'" cols="'.$k[5][0].'" rows="'.$k[5][1].'">'.$render_value[$n].'</textarea></td>'."\n";
 			break;
 		case 'option': //option
 			$render_element[$n] = array() ;
-			$render_element_arr[] .= "\t".'<td class="style_td"><select id="PKWKMAIL_'.$attr_name[$n].'" name="PKWKMAIL_'.$attr_name[$n].'">'."\n";
+			$render_element_arr[] = "\t".'<td class="style_td"><select id="PKWKMAIL_'.$attr_name[$n].'" name="PKWKMAIL_'.$attr_name[$n].'">'."\n";
 			foreach($k[3] as $value_arr){
 				$value_arr = htmlspecialchars($value_arr, ENT_QUOTES);
 				$selected = ($value_arr == $render_value[$n]) ? ' selected="selected"' : '';
-				$render_element_arr[] .= "\t\t".'<option value="'.$value_arr.'"'.$selected.'>'.$value_arr."</option>\n";
+				$render_element_arr[] = "\t\t".'<option value="'.$value_arr.'"'.$selected.'>'.$value_arr."</option>\n";
 			}
-			$render_element_arr[] .= "\t".'</select></td>'."\n";
+			$render_element_arr[] = "\t".'</select></td>'."\n";
 			$render_element_arr_num = count($render_element_arr);
 			for($i=0;$i<$render_element_arr_num;++$i){
-				$render_element[$n][] .= $render_element_arr[$i];
+				$render_element[$n][] = $render_element_arr[$i];
 			}
 			break;
 		case 'radio':
 		case 'radio-br':
 			//radio
 			$render_element[$n] = array() ;
-			$render_element_arr[] .= "\t".'<td class="style_td">'."\n";
+			$render_element_arr[] = "\t".'<td class="style_td">'."\n";
 			foreach($k[3] as $value_arr){
 				$value_arr = htmlspecialchars($value_arr, ENT_QUOTES);
 				$checked = ($value_arr == $render_value[$n]) ? ' checked="checked"' : '';
-				$render_element_arr[] .= "\t\t".'<label><input type="radio" name="PKWKMAIL_'.$attr_name[$n].
+				$render_element_arr[] = "\t\t".'<label><input type="radio" name="PKWKMAIL_'.$attr_name[$n].
 						'" value="'.$value_arr.'"'.$checked.'/>'.$value_arr.'</label>'.$radio_break."\n";
 			}
-			$render_element_arr[] .= "\t".'</td>'."\n";
+			$render_element_arr[] = "\t".'</td>'."\n";
 			$render_element_arr_num = count($render_element_arr);
 			for($i=0;$i<$render_element_arr_num;++$i){
-				$render_element[$n][] .= $render_element_arr[$i];
+				$render_element[$n][] = $render_element_arr[$i];
 			}
 			break;
 		case 'checkbox':
 		case 'checkbox-br':
 			//checkbox
 			$render_element[$n] = array() ;
-			$render_element_arr[] .= "\t".'<td class="style_td">'."\n";
+			$render_element_arr[] = "\t".'<td class="style_td">'."\n";
 			foreach($k[3] as $value_arr) {
 				$value_arr = htmlspecialchars($value_arr, ENT_QUOTES);
 				$chked = false;
@@ -379,13 +379,13 @@ function PKWKMAIL_formmaker($attr,$cnfm, $a_page)
 					}
 				}
 				$checked = ($chked) ? ' checked="checked"' : '';
-				$render_element_arr[] .= "\t\t".'<label><input type="checkbox" name="PKWKMAIL_'.$attr_name[$n].
+				$render_element_arr[] = "\t\t".'<label><input type="checkbox" name="PKWKMAIL_'.$attr_name[$n].
 					'[]" value="'.$value_arr.'"'.$checked.'/>'.$value_arr.'</label>'.$checkbox_break."\n";
 			}
-			$render_element_arr[] .= "\t".'</td>'."\n";
+			$render_element_arr[] = "\t".'</td>'."\n";
 			$render_element_arr_num = count($render_element_arr);
 			for($i=0;$i<$render_element_arr_num;++$i) {
-				$render_element[$n][] .= $render_element_arr[$i];
+				$render_element[$n][] = $render_element_arr[$i];
 			}
 			break;
 		}
