99a3f4ffe0cd272f2766db927c511a742dbca519
Shun Hotta <stars.big2men@gmail.com>
2024-12-13 22:57:31 +0900

    Fix fatal error
    QdmailComponent smtpObject の定義を修正

--- lib/qdmail.php
+++ lib/qdmail.php
@@ -3722,7 +3722,7 @@ class QdmailComponent extends QdmailUserFunc{
 	//----------------------------
 	// Override Parent Method
 	//----------------------------
-	function & smtpObject(){
+	function & smtpObject( $null = false ){
 		if( isset( $this->Qdsmtp ) && is_object( $this->Qdsmtp ) ){
 			return $this->Qdsmtp;
 		}
