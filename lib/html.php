4042497f559a4969edb12a2d283777e93b732538
Shun Hotta <stars.big2men@gmail.com>
2025-01-28 22:08:13 +0900

    ブログサムネイル設定セクションからQHMの文字を削除

--- lib/html.php
+++ lib/html.php
@@ -727,21 +727,18 @@ $template
           <label class="control-label col-sm-2">記事の内容</label>
           <div class="controls col-sm-10">
               <textarea name="msg" id="msg" tabindex="4" rows="20" class="form-control">$s_postdata</textarea>
-  		</div>
-  	</div>
-      <div class="form-group">
-          <div class="controls col-sm-10 col-sm-offset-2">
-	  		<a class="show-thumbnail" href="#">サムネイルを指定する &gt;&gt;</a>
-  			<div class="set-thumbnail">
-  				<small>自動で本文の画像が使われます。<br />特別に指定したい場合、画像を画像名またはURLで指定してください。</small>
-                  <p style="color:#333;">画像名またはURL：<input type="text" name="image" value="{$data['image']}" tabindex="5" class="form-control" /></p>
-  				<p><small><span class="swfu"><a href="swfu/index_child.php">&gt;&gt;QHMのファイル管理（SWFU）を使って画像をアップする</a></span></small></p>
-  			</div>
-<!--  			<span class="swfu"><a href="swfu/index_child.php"><i class="icon-picture"></i>SWFU</a><span>
-			<p class="help-block">SWFUの画像を使う場合、画像詳細画面の<b>URL</b>をコピペしてください。</p>
--->
-  		</div>
-  	</div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="controls col-sm-10 col-sm-offset-2">
+        <a class="show-thumbnail" href="#">サムネイルを指定する &gt;&gt;</a>
+        <div class="set-thumbnail">
+          <small>自動で本文の画像が使われます。<br />特別に指定したい場合、画像を画像名またはURLで指定してください。</small>
+          <p style="color:#333;">画像名またはURL：<input type="text" name="image" value="{$data['image']}" tabindex="5" class="form-control" /></p>
+          <p><small><span class="swfu"><a href="swfu/index_child.php">&gt;&gt;ファイル管理（SWFU）を使って画像をアップする</a></span></small></p>
+        </div>
+      </div>
+    </div>
       <div class="form-group">
         <div class="col-sm-10 col-sm-offset-2">
           <div style="float:{$buttons_align};">
